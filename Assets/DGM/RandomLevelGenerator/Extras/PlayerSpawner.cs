51d9723137779a8edade189ded512ad62682e5e5
DevGa_me <devm19102003@gmail.com>
2022-04-12 15:48:47 +0530

    Added sample player spawning

--- Assets/DGM/RandomLevelGenerator/Extras/PlayerSpawner.cs
+++ Assets/DGM/RandomLevelGenerator/Extras/PlayerSpawner.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
+    [Tooltip("Player Gameoject to spawn")]
     [SerializeField] GameObject playerObject = default;
 
+    [Tooltip("The position buffer applied when spawning the player.\n(0, 0) means that player will be spawned at the top-left corner of the starting tile")]
     [SerializeField] Vector2 buffer = default;
-    [SerializeField] CameraController cameraController;
 
     private void OnEnable()
     {
+        //Listen to the LevelGenerated event to spawn the player when the level is completely generated
         LevelGenerator.ELevelGenerated += SpawnPlayer;
     }
 
@@ -23,6 +25,7 @@ public class PlayerSpawner : MonoBehaviour
         Debug.Log($"Ending Coordinates are : {endingCoordinates}");
 
         GameObject player = Instantiate<GameObject>(playerObject, new Vector3(startingCoordinates.x * 20 + buffer.x, (startingCoordinates.y * 20 + buffer.y) * -1, 0), Quaternion.identity);
-        cameraController.target = player.transform;
+
+        Debug.Log($"Player was spawned at {player.transform.position.ToString()}");
     }
 }
